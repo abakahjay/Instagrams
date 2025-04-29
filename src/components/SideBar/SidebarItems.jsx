d6de23014b15c9c6180465059cc19c16e57c7280
abakahjay <joshuaabakahhh@gmail.com>
2025-04-29 03:36:05 +0000

    First

---
+++ src/components/SideBar/SidebarItems.jsx
@@ -0,0 +1,21 @@
+import CreatePost from "./CreatePost";
+import Home from "./Home";
+import Notifications from "./Notifications";
+import ProfileLink from "./ProfileLink";
+import Search from "./Search";
+import Messages from './MessagesLink'
+
+const SidebarItems = ({authUser,onLogout}) => {
+	return (
+		<>
+			<Home authUser={authUser} onLogout={onLogout} />
+			<Search authUser={authUser} onLogout={onLogout}/>
+			<Notifications authUser={authUser} onLogout={onLogout}/>
+			<CreatePost authUser={authUser} onLogout={onLogout}/>
+			<Messages authUser={authUser} onLogouot={onLogout}/>
+			<ProfileLink authUser={authUser} onLogout={onLogout} />
+		</>
+	);
+};
+
+export default SidebarItems;
